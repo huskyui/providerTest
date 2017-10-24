23be3518c89ddf4ee983cc7ebbd16a6c20176253
husky <2207019991@qq.com>
2017-10-24 10:22:45 +0800

    first commit

---
+++ app/src/main/java/com/example/android/providertest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.providertest;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button queryData = (Button)findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.example.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                if(cursor != null){
+                    while(cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        Log.d("MainActivity","book name is"+name);
+                        Log.d("MainActivity","book author is"+author);
+                    }
+                    cursor.close();
+                }
+
+            }
+        });
+
+    }
+}
